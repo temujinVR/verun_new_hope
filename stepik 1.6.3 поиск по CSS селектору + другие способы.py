5f7c9b5827b7a57f98908b698a255ddc5fcc550c
temujinTF <110490614+temujinTF@users.noreply.github.com>
2022-08-03 20:36:57 +0300

    Add files via upload

---
+++ stepik 1.6.3 поиск по CSS селектору + другие способы.py
@@ -0,0 +1,61 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://suninjuly.github.io/simple_form_find_task.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    input1 = browser.find_element(By.TAG_NAME, "input")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element(By.NAME, "last_name")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element(By.CLASS_NAME, "form-control.city")
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element(By.ID, "country")
+    input4.send_keys("Russia")
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
+
+
+"""
+link = "http://suninjuly.github.io/simple_form_find_task.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    input1 = browser.find_element(by='tag name',value='value1')
+    print(input1)
+    input1.send_keys("Ivan")
+    input2 = browser.find_element(by='name', value='value2')
+    print(input2)
+    input2.send_keys("Petrov")
+    input3 = browser.find_element(by='class name', value='value3')
+    print(input3)
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element(by='id',value='value4')
+    print(input4)
+    input4.send_keys("Russia")
+    button = browser.find_element(by='css selector', value="button.btn")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
+"""
+# другая версия поиска элементов
